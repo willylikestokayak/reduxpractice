84ddd45c7567d7d7404ab8309f21a61d76e9bacf
willylikestokayak <wccoker@gmail.com>
2018-04-18 15:50:57 -0700

    setting state on search_bar.js component

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyCegKd2CfaWWI5xFYqludDONoqEzgMFx0A';
 
 const App = () => {
-	return <div>Hi!</div>;
+	return (
+	<div>
+		<SearchBar />
+	</div>
+	);
+
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
